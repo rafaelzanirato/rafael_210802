211c9e61ee29dc149602cd9420406cbd33698f2f
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-08-02 20:02:13 -0300

    Add files via upload

---
+++ rafael_210802.py
@@ -0,0 +1,16 @@
+print("Question 2 ************************")
+for number in range(1,21):
+    print(number)
+print("************************")
+for number in range(1,21):
+    if number < 4:
+        print(number)
+    elif number >= 4 and number < 8:
+        print(number)
+    elif number >= 8 and number < 12:
+        print(number)
+    elif number >= 12 and number < 16:
+        print(number)
+    else:
+        print (number)
+
